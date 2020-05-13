76820e357fd6ef41612d417bd1442637c0595c8f
hyonlee42 <hyon.lee@utexas.edu>
2020-05-13 17:48:57 -0500

    add stock quote search to navbar and functionality

--- quotes/views.py
+++ quotes/views.py
@@ -5,15 +5,24 @@ def home(request):
     import requests
     import json
     
-    #pk_3bcd7eb9917441f99e7dab265e023645
-    api_request = requests.get("https://cloud.iexapis.com/stable/stock/aapl/quote?token=pk_3bcd7eb9917441f99e7dab265e023645")
+    if request.method == 'POST':
+      ticker = request.POST['ticker']
+      api_request = requests.get("https://cloud.iexapis.com/stable/stock/" + ticker + "/quote?token=pk_3bcd7eb9917441f99e7dab265e023645")
 
-    try:
+      try:
         api = json.loads(api_request.content)
-    except Exception as e:
+      except Exception as e:
         api = "Error..."
+      return render(request, 'home.html', {'api': api})
 
-    return render(request, 'home.html', {'api': api})
+    else:
+      return render(request, 'home.html', {'ticker': "Enter a Ticker Symbol Above..."})
+
+
+
+
+
+
 
 
 def about(request):
