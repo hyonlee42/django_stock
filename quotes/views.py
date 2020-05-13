8f71ec57c412499fea14a79c6f2983410c37eb6f
hyonlee42 <hyon.lee@utexas.edu>
2020-05-13 17:04:46 -0500

    connect api

--- quotes/views.py
+++ quotes/views.py
@@ -2,7 +2,18 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'home.html', {})
+    import requests
+    import json
+    
+    #pk_3bcd7eb9917441f99e7dab265e023645
+    api_request = requests.get("https://cloud.iexapis.com/stable/stock/aapl/quote?token=pk_3bcd7eb9917441f99e7dab265e023645")
+
+    try:
+        api = json.loads(api_request.content)
+    except Exception as e:
+        api = "Error..."
+
+    return render(request, 'home.html', {'api': api})
 
 
 def about(request):
