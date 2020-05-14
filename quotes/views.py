946f4cbd0e15ae3b95102572014cba773d432771
hyonlee42 <hyon.lee@utexas.edu>
2020-05-14 11:16:05 -0500

    delete stocks from portfolio option

--- quotes/views.py
+++ quotes/views.py
@@ -43,3 +43,10 @@ def add_stock(request):
   else:
     ticker = Stock.objects.all()
     return render(request, 'add_stock.html', {'ticker': ticker})
+
+
+def delete(request, stock_id):
+  item = Stock.objects.get(pk=stock_id)
+  item.delete()
+  messages.success(request, ("Stock Has Been Deleted!"))
+  return redirect(add_stock)
